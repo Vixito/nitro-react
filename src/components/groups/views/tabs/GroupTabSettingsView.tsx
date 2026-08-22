a0d42d4b9e24908794b5f41ec41d6171b651fb4e
Vixito <carlosandres81182@gmail.com>
2026-08-22 02:13:55 -0500

    fix(ui): fix form-check squishing with fixed 15px aspect ratio and improve settings tab layout

--- src/components/groups/views/tabs/GroupTabSettingsView.tsx
+++ src/components/groups/views/tabs/GroupTabSettingsView.tsx
@@ -58,15 +58,15 @@ export const GroupTabSettingsView: FC<GroupTabSettingsViewProps> = props =>
     }, [ setCloseAction, saveSettings ]);
     
     return (
-        <Column overflow="auto">
-            <Column>
+        <Column overflow="auto" gap={ 2 }>
+            <Column gap={ 2 }>
                 { STATES.map((state, index) =>
                 {
                     return (
-                        <Flex key={ index } alignItems="center" gap={ 1 }>
-                            <input className="form-check-input" type="radio" name="groupState" checked={ (groupState === index) } onChange={ event => setGroupState(index) } />
-                            <Column gap={ 0 }>
-                                <Flex gap={ 1 }>
+                        <Flex key={ index } alignItems="start" gap={ 2 }>
+                            <input className="form-check-input mt-1" type="radio" name="groupState" checked={ (groupState === index) } onChange={ event => setGroupState(index) } />
+                            <Column gap={ 0 } className="min-w-0">
+                                <Flex alignItems="center" gap={ 1 }>
                                     <i className={ `icon icon-group-type-${ index }` } />
                                     <Text bold>{ LocalizeText(`group.edit.settings.type.${ state }.label`) }</Text>
                                 </Flex>
@@ -76,10 +76,10 @@ export const GroupTabSettingsView: FC<GroupTabSettingsViewProps> = props =>
                     );
                 }) }
             </Column>
-            <HorizontalRule />
-            <Flex alignItems="center" gap={ 1 }>
-                <input className="form-check-input" type="checkbox" checked={ groupDecorate } onChange={ event => setGroupDecorate(prevValue => !prevValue) } />
-                <Column gap={ 1 }>
+            <HorizontalRule className="my-1" />
+            <Flex alignItems="start" gap={ 2 }>
+                <input className="form-check-input mt-1" type="checkbox" checked={ groupDecorate } onChange={ event => setGroupDecorate(prevValue => !prevValue) } />
+                <Column gap={ 0 } className="min-w-0">
                     <Text bold>{ LocalizeText('group.edit.settings.rights.caption') }</Text>
                     <Text>{ LocalizeText('group.edit.settings.rights.members.help') }</Text>
                 </Column>
